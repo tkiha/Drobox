a83d493ab788bb41bc476da310e0795a516c363a
kihara tadahiro <pltokyo@kihara-no-Mac-mini.local>
2014-11-27 14:03:56 +0900

    フォルダのリンクを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
     return 'ファイル' if target_object.kind_of?(Upfile)
     '不明'
   end
+
+  def folder_parents_html(folder)
+    parents ||= []
+    folder_parents(folder,parents)
+    content_tag(:folders) do
+      parents.each_with_index.reverse_each do |f,index|
+        concat link_to(f.name,list_folder_path(f))
+        concat '  ->  ' if index!=0
+      end
+    end
+  end
+
+  # 受け取ったフォルダの親を辿ってparents配列にセットする
+  def folder_parents(folder,parents)
+    parents << folder
+    parent = folder.parent_folder
+    folder_parents(parent,parents) if parent.present?
+
+  end
+
 end
