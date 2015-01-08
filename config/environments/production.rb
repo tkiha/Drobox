5c298ac2fb15c0050ce93113d5ed1a62dce13101
kihara tadahiro <pltokyo@kihara-no-Mac-mini.local>
2015-01-08 14:21:49 +0900

    for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
 # Default Mailer Host
   Rails.application.routes.default_url_options[:host] = 'domain.com'
 end
