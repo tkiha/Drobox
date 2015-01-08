fa2586277100226a31c501185f7bf8f8f00dd146
kihara tadahiro <pltokyo@kihara-no-Mac-mini.local>
2015-01-08 13:23:36 +0900

    routeのハッシュ表記の修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,20 @@ Rails.application.routes.draw do
   devise_scope :user do
     authenticated :user do
       # 認証済みユーザーの場合はファイル一覧へ
-      root :to => 'folders#show'
+      root to: 'folders#show'
     end
     unauthenticated :user do
-      root :to => 'devise/registrations#new', as: :unauthenticated_root
+      root to: 'devise/registrations#new', as: :unauthenticated_root
     end
   end
 
   # idを省略可能とした。省略時はroot扱いとなる
-  match 'items(/:id)' => 'folders#show', :as => 'items', :defaults => {id: nil}, :via => :get
-  match 'search(/:id)' => 'folders#search', :as => 'search_folder', :defaults => {id: nil}, :via => :get
+  match 'items(/:id)' => 'folders#show', as: 'items', defaults: {id: nil}, via: :get
+  match 'search(/:id)' => 'folders#search', as: 'search_folder', defaults:  {id: nil}, via: :get
 
   # folder_idはどのフォルダ一覧へ戻るかを示すためのキー。省略可能。省略時はrootへ戻る事になる
-  match 'find(/:folder_id)' => 'find#new', :as => 'find', :defaults => {folder_id: nil}, :via => :get
-  match 'findresult(/:folder_id)' => 'find#show', :as => 'find_result', :defaults => {folder_id: nil}, :via => :post
+  match 'find(/:folder_id)' => 'find#new', as: 'find', defaults: {folder_id: nil}, via: :get
+  match 'findresult(/:folder_id)' => 'find#show', as: 'find_result', defaults: {folder_id: nil}, via: :post
 
   resources :folders, only: [] do
     resources :upfiles, only: [:edit, :create, :new, :destroy, :update, :show] do
