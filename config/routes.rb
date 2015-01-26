a4a85ebd1648557a941d02e0406a4b75b486ab15
Akihiro Matsumura <matsumura.aki@gmail.com>
2015-01-26 20:16:02 +0900

    ARのモデルのオブジェクトにたいして、resourcesで書くようにする

--- config/routes.rb
+++ config/routes.rb
@@ -13,18 +13,23 @@ Rails.application.routes.draw do
   end
 
   # idを省略可能とした。省略時はroot扱いとなる
-  match 'items(/:id)' => 'folders#show', as: 'items', defaults: {id: nil}, via: :get
-  match 'search(/:id)' => 'folders#search', as: 'search_folder', defaults:  {id: nil}, via: :get
+  get 'items(/:id)' => 'folders#show', as: 'items', defaults: {id: nil} # folders の show でOK
+  get 'search(/:id)' => 'folders#search', as: 'search_folder', defaults:  {id: nil} # => folders の member に移動
 
   # folder_idはどのフォルダ一覧へ戻るかを示すためのキー。省略可能。省略時はrootへ戻る事になる
-  match 'find(/:folder_id)' => 'find#new', as: 'find', defaults: {folder_id: nil}, via: :get
-  match 'findresult(/:folder_id)' => 'find#show', as: 'find_result', defaults: {folder_id: nil}, via: :post
+  get 'find(/:folder_id)' => 'find#new', as: 'find', defaults: {folder_id: nil}
+  post 'find(/:folder_id)' => 'find#show', as: 'find_result', defaults: {folder_id: nil}
 
-  resources :folders, only: [] do
+  resources :folders, only: [:show] do
+    member do
+      # /folders/3/search | /search/3
+      get :search
+    end
     resources :upfiles, only: [:edit, :create, :new, :destroy, :update, :show] do
       member do
         get :download
         post :move, :copy
+        # resources :fileshares 下3行をまとめたい
         get :newshares, to: 'fileshares#new'
         patch :updateshares, to: 'fileshares#update'
         delete :destroyshares, to: 'fileshares#destroy'
@@ -38,6 +43,7 @@ Rails.application.routes.draw do
     resources :folders, controller: :subfolders, only: [:edit, :create, :new, :destroy, :update, :show] do
       member do
         post :move, :copy
+        # resources :shares にしたい
         get :newshares, to: 'foldershares#new'
         patch :updateshares, to: 'foldershares#update'
         delete :destroyshares, to: 'foldershares#destroy'
@@ -49,12 +55,12 @@ Rails.application.routes.draw do
   end
 
   namespace 'fromshare' do
-    resource :items, only: [:show]
+    resources :items, only: [:index]
   end
 
   namespace 'toshare' do
-    resource :items, only: [:show]
-    resources :folder_items, only: [:show]
+    resource :items, only: [:show] # resources :items, only: [:index] 上と同じにしたい
+    resources :folder_items, only: [:show] # folders にしたい
   end
 
   resources :events, only: [:index]
